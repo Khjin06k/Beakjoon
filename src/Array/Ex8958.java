4ee90a91d559f97e984627b8ba37320b44889ce7
김희진 <khj4209kk@gmail.com>
2022-09-18 13:41:48 +0900

    Ex8958 해결

--- src/Array/Ex8958.java
+++ src/Array/Ex8958.java
@@ -5,28 +5,22 @@ public class Ex8958 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        String[] arr = new String[N];
-        //배열 입력 받음
-        int count[] = new int[80];
-        int sum = 0;
-        //배열 입력 받기
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextLine();
+        String[] arr = new String[N];//N 크기의 배열을 생성 및 입력
+        for(int i=0; i<N; i++){ //N개의 문자열을 입력받음
+            arr[i] = sc.next();
         }
-        System.out.println("");
-        //인덱스0~N 하나씩 처리 진행;
-        for (int i = 0; i < N; i++) { //N만큼 반복 진행
-            String[] result = arr[i].split(""); //각각 split을 통해 O,X 구별하여 배열에 추가
-            for (int j = 1; j < result.length; j++) {
-                if (result[0] == "O") count[0] = 1; //0번째 인덱스는 따로 값 저장
-                else count[0] = 1;
 
-                if (result[i] == "X") count[i] = 0; //만약 result가 X라면 0을 count에 저장
-                else count[i] = count[i - 1] + 1; // O라면 그 이전 값에 +1후 저장
-            }
-            System.out.println(result.length);
-            for (int t = 0; t < result.length; t++) {
-                sum = sum + count[t]; //count 배열이 가진 모든 숫자를 합함
+        for(int i = 0; i<N; i++){//N번의 반복 진행으로 각각의 케이스 점수 출력
+            int count = 0; //각각의 점수
+            int sum = 0; // 각 케이스 점수의 합
+            //각 케이스를 반복할 때마다 초기화가 된 상태여야 하기 때문에 반복문 내부에 선언함
+            for(int j = 0; j<arr[i].length(); j++) {
+                if (arr[i].charAt(j) == 'O'){
+                    count++;
+                    sum +=count; //O일 경우 count를 증가시켜 sum에 더함
+                }else{
+                    count = 0;//만약 X를 만났을 경우 count를 다시 초기화 시킴
+                }
             }
             System.out.println(sum);
         }
